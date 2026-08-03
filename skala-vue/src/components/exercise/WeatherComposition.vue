55c49863e0565325bbe8f596602df88f51eeaf4e
kksj216 <kwonsujin216@gmail.com>
2026-08-03 17:47:41 +0900

    add temperature update

--- skala-vue/src/components/exercise/WeatherComposition.vue
+++ skala-vue/src/components/exercise/WeatherComposition.vue
@@ -5,7 +5,7 @@ import { ref, computed, watch, watchEffect } from 'vue'
 const weatherList = ref([
   { id: 'city_01', name: '서울', temp: 28, status: '맑음' },
   { id: 'city_02', name: '수원', temp: 24, status: '비' },
-  { id: 'city_03', name: '부산', temp: 26, status: '구름' },
+  { id: 'city_03', name: '부산', temp: 17, status: '구름' },
 ])
 
 // 2. [1일차 데이터] 검색어 및 알림창 제어용 데이터
