b15332b9b70740fffe48e8212c0e9753fb362a6f
kksj216 <kwonsujin216@gmail.com>
2026-08-04 15:43:14 +0900

    add pinia counter

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,9 +4,10 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
+  const tripleCount = computed(() => count.value * 3)
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  return { count, doubleCount, tripleCount, increment }
 })
